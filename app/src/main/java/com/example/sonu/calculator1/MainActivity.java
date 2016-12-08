f9695f7b340d1769c2136816ac32ee0736522e33
Sid <f2016097@goa.bist-pilani.ac.in>
2016-12-08 21:53:50 +0530

    Initial commit

---
+++ app/src/main/java/com/example/sonu/calculator1/MainActivity.java
@@ -0,0 +1,67 @@
+package com.example.sonu.calculator1;
+
+import android.os.StrictMode;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+
+public class MainActivity extends AppCompatActivity {
+
+
+    String total="";
+    double v1, v2;
+    String sign="";
+
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void OnClick(View v){
+
+        Button button =(Button)v;
+        String str=button.getText().toString();
+        total=str;
+        EditText edit=(EditText)findViewById(R.id.editText);
+        edit.setText(total);
+    }
+    public void OnAdd(View v) {
+        v1 = Double.parseDouble(total);
+        total = "";
+        Button button = (Button) v;
+        String str = button.getText().toString();
+        sign = str;
+        EditText edit = (EditText)findViewById(R.id.editText);
+        edit.setText("");}
+
+
+
+    public void OnCalculate(View v){
+EditText edit=(EditText)findViewById(R.id.editText);
+
+        String str2 = edit.getText().toString();
+        v2 = Double.parseDouble(str2);
+        double grand_total = 0;
+        if(sign.equals("+")){
+            grand_total=v1+v2;
+        }
+        else if(sign.equals("-")){
+            grand_total=v1-v2;
+        }
+        else if(sign.equals("*")){
+            grand_total=v1*v2;
+        }
+        else if(sign.equals("/")){
+            grand_total=v1/v2;
+        }edit.setText(grand_total+"");
+    }
+    public void OnClear(View v)
+
+    {
+        EditText edit=(EditText)findViewById(R.id.editText);
+        edit.setText("");
+    }
+}
